72731bed06de4380da85135e60251213c5e7d9c5
timlock <81235388+timlock@users.noreply.github.com>
2022-09-05 12:38:20 +0200

    Mergeconflict reste

--- Space-Invaders/src/Postprocessing.cpp
+++ Space-Invaders/src/Postprocessing.cpp
@@ -31,6 +31,7 @@ void PostprocessingBildInvertierenShader::activate(const BaseCamera& Cam) const
 {
 	BaseShader::activate(Cam);
 
+	Tex->activate();
 	setParameter(Tex0Loc, (int)Tex->ID());
 }
 
@@ -42,7 +43,7 @@ void PostprocessingBildInvertierenShader::setTex0(Texture* Tex)
 Postprocessing::Postprocessing(unsigned int Width, unsigned int Height): Shader(new PostprocessingBildInvertierenShader())
 {
 	// TODO: kein plan ob GL para richtig
-	bool created = this->renderetScene.create(Width, Height, GL_R32F, GL_RED, GL_FLOAT, GL_NEAREST, GL_NEAREST, GL_CLAMP_TO_EDGE, false);
+	bool created = this->renderetScene.create(Width, Height, GL_RGBA16F, GL_RGBA, GL_FLOAT, GL_LINEAR, GL_LINEAR, GL_CLAMP_TO_EDGE, false);
 	if (!created)
 		throw std::exception();
 
@@ -60,11 +61,12 @@ Postprocessing::~Postprocessing()
 void Postprocessing::activate()
 {
 	// TODO: Bin mir nicht sicher ob glClearColor und glCullFace hier schlau ist
-	glClearColor(1.0f, 0.0, 0.0f, 1);
-	glCullFace(GL_FRONT);
+	//glClearColor(1.0f, 0.0, 0.0f, 1);
+	//glCullFace(GL_FRONT);
 
 	FrameBuffer.attachColorTarget(renderetScene);
 	FrameBuffer.activate();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void Postprocessing::deactivate()
@@ -82,7 +84,7 @@ void Postprocessing::drawPost(const BaseCamera& Cam)
 
 	Shader->activate(Cam);
 
-	renderetScene.activate();
+	//renderetScene.activate();
 	
 	VB.activate();
 	
@@ -90,7 +92,7 @@ void Postprocessing::drawPost(const BaseCamera& Cam)
 	
 	VB.deactivate();
 
-	renderetScene.deactivate();
+	//renderetScene.deactivate();
 
 	Shader->deactivate();
 }
